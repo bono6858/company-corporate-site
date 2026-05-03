111a224341ba86c89240d147392ed78cb5c837e5
bono6858 <contact@b-bonobo.co.jp>
2026-05-04 02:49:00 +0900

    Update news.js

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -1,1 +1,26 @@
+const fetch = require('node-fetch');
 
+exports.handler = async () => {
+  const endpoint = 'https://【サービスID】.microcms.io/api/v1/news';
+
+  try {
+    const res = await fetch(endpoint, {
+      headers: {
+        'X-MICROCMS-API-KEY': process.env.MICROCMS_API_KEY
+      }
+    });
+
+    const data = await res.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch' })
+    };
+  }
+};
